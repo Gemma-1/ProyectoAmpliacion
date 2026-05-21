59d0fb534bcce55398512ee0e6ea61e5e27c2fb5
Gemma-1 <gemagithub@gmail.com>
2026-05-21 11:23:07 +0000

    modificado

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "<li>Marta Pérez García</li>";
 echo "<li>Laura Gema Moreno Llarena</li>";
 echo "</ul>";
 echo "<p>Entorno WordPress desplegado con éxito mediante Vagrant, Docker y Git.</p>";
-echo "<p>...............................PROYECTO.......................................</p>";
+echo "<p>......................................................................</p>";
 
 echo "<p>Para ver las modificaciones en sus ficheros solo debe ejecutar vagrant provision en su terminal y ¡LISTO!</p>";
 ?>
